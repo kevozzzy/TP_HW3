70a4b4a98426398cbd1785b2f1d65747b3b64940
Аитов Тимерлан <traitov@edu.hse.ru>
2026-06-14 18:04:07 +0300

    Add reporter and runner commands

--- run.sh
+++ run.sh
@@ -16,6 +16,15 @@ case "$1" in
         mkdir -p local_data
         python3 generator/generate.py local_data
         ;;
+    build_reporter)
+        docker build -t hw-reporter ./reporter
+        ;;
+    run_reporter)
+        mkdir -p data
+        docker run --rm \
+            -v "$(pwd)/data:/data" \
+            hw-reporter
+        ;;
     *)
         echo "Неизвестная комнада"
         exit 1
